62b2406044689762c4324c494586dff67ccf9b41
harleqat <harleqat@gmail.com>
2017-01-16 14:51:38 -0500

    tested

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+	res = {}
+	data.each do |key, values|
+		values.each do |char, names|
+			names.each do |name|
+				if !res[name]
+					res[name] = {}
+				end
+				if !res[name][key]
+					res[name][key] = []
+				end
+				res[name][key].push(char.to_s)
+			end
+		end
+	end
+	res
 end
+
+
+# pigeon_data = {
+#   :color => {
+#     :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#     :brown => ["Queenie", "Alex"]
+#   },
+#   :gender => {
+#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#     :female => ["Queenie", "Ms. K"]
+#   },
+#   :lives => {
+#     "Subway" => ["Theo", "Queenie"],
+#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#     "Library" => ["Peter Jr."],
+#     "City Hall" => ["Andrew"]
+#   }
+# }
+
+# nyc_pigeon_organizer(pigeon_data)
